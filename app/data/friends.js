05f705c31decedfb964408227203efc758767974
Randall Ward <randallwward@gmail.com>
2016-07-23 16:36:09 -0500

    corrections

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 var friends = [
-{ name: 'Kayne West',
-  photo:'http://pixel.nymag.com/imgs/daily/vulture/2016/02/02/2-kanye-west-album.w529.h529.jpg'
-  scores: [5,5,2,3,1,1,1,5,5,4] 
+{ "name": 'Kayne West',
+  "photo":'http://pixel.nymag.com/imgs/daily/vulture/2016/02/02/2-kanye-west-album.w529.h529.jpg'
+  "scores": [5,5,2,3,1,1,1,5,5,4] 
   },{
-  name: 'John Cena',
-  photo:'http://www.ew.com/sites/default/files/styles/tout_image_612x380/public/i/2015/11/18/john-cena_0.jpg?itok=0OcLxFXC'
-  scores: [5,5,5,5,1,5,3,3,3,3]
+  "name": 'John Cena',
+  "photo":'http://www.ew.com/sites/default/files/styles/tout_image_612x380/public/i/2015/11/18/john-cena_0.jpg?itok=0OcLxFXC'
+  "scores": [5,5,5,5,1,5,3,3,3,3]
   },{
-  name: 'Dwayne Johnson',
-  photo:'http://a4.files.biography.com/image/upload/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE5NDg0MDU1MjM5ODg2MzUx.jpg'
-  scores: [3,3,3,3,3,3,3,3,3,3]
+  "name": 'Dwayne Johnson',
+  "photo":'http://a4.files.biography.com/image/upload/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE5NDg0MDU1MjM5ODg2MzUx.jpg'
+  "scores": [3,3,3,3,3,3,3,3,3,3]
   },{
-  name: 'Takeshi Kitano',
-  photo:'http://www.somethinkblue.com/upd_files/extra_images/Kitano-4.jpg'
-  scores: [1,5,1,1,1,5,2,2,1,1]
+  "name": 'Takeshi Kitano',
+  "photo":'http://www.somethinkblue.com/upd_files/extra_images/Kitano-4.jpg'
+  "scores": [1,5,1,1,1,5,2,2,1,1]
   }];
